2d57a7ae2160fe143a486a678c8489f295bc2628
jknguyen22 <jen.nguyen916@gmail.com>
2019-09-05 04:43:44 -0500

    add create react app

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';  //parent component wrapper
+
+ReactDOM.render(<App />, document.getElementById('root')); //grabbing ./public/index.html root div
